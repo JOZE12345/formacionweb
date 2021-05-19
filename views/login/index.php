cd46cab6421d150378b11d24859b9cb2478f1d6e
JOZE12345 <ja8228228@gmial.com>
2021-05-19 17:02:33 -0500

    antonio

--- views/login/index.php
+++ views/login/index.php
@@ -3,11 +3,16 @@
 	require_once("views/header.php");
 
 ?>
+
+<div class="img-fluid" style="max-width:100%; height: auto; background-image: url('public/img/login.jpg'); background-repeat: no-repeat;
+background-position: center; background-size: cover; background-attachment: fixed;">
+
 <div class="container">
 	<div class="d-flex justify-content-center h-100">
-		<div class="card">
+		
+		<div class="card">		
 			<div class="card-header">
-				<h3>Sign In</h3>
+				<h3>Iniciar Sesion</h3>
 				<div class="d-flex justify-content-end social_icon">
 					<span><i class="fab fa-facebook-square"></i></span>
 					<span><i class="fab fa-google-plus-square"></i></span>
@@ -30,7 +35,7 @@
 						<input type="password" class="form-control" placeholder="password">
 					</div>
 					<div class="row align-items-center remember">
-						<input type="checkbox">Remember Me
+						<input type="checkbox">Recordar contraseña
 					</div>
 					<div class="form-group">
 						<input type="submit" value="Login" class="btn float-right login_btn">
@@ -39,10 +44,10 @@
 			</div>
 			<div class="card-footer">
 				<div class="d-flex justify-content-center links">
-					Don't have an account?<a href="#">Sign Up</a>
+					No estas registrado?<a href="#">Crear cuenta nueva</a>
 				</div>
 				<div class="d-flex justify-content-center">
-					<a href="#">Forgot your password?</a>
+					<a href="#">Olvidaste tu contraseña?</a>
 				</div>
 			</div>
 		</div>
