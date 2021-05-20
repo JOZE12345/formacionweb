dd1d56a89806db91b5828953a2679f6f670f1541
JOZE12345 <ja8228228@gmial.com>
2021-05-20 17:50:39 -0500

    antonio

--- views/header.php
+++ views/header.php
@@ -12,14 +12,16 @@
     <link rel="stylesheet" href="<?php echo constant('URL') ?>public/css/footer.css">
     <link rel="stylesheet" href="<?php echo constant('URL') ?>public/css/hover.css">
     <link rel="stylesheet" href="<?php echo constant('URL') ?>public/css/regis.css">
+    <link rel="stylesheet" href="<?php echo constant('URL') ?>public/css/ubicaciones.css">
     <link rel="shortcut icon" href="<?php echo constant('URL') ?>public/img/pvsac1.ico">
- 
     <link rel="shortcut icon" href="<?php echo constant('URL') ?>public/img/navbar2.ico">
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="<?php echo constant('URL') ?>public/js/registro.js"></script>
+<script src="<?php echo constant('URL') ?>public/js/ubicaciones.js"></script>
+
 
         <title>RESTAURANTE </title>
   </head>
