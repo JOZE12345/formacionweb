bb258fc14f807d13bd3ba7f958085d3f65b2d50b
yairguevara12 <guevaradelgadoyair12@gmial.com>
2021-05-20 17:51:26 -0500

    y

--- views/header.php
+++ views/header.php
@@ -20,7 +20,9 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="<?php echo constant('URL') ?>public/js/registro.js"></script>
-
+<script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCmK6LbeTBpHyA1YufHerk9iiY2fatiEiY&callback=initMap">
+    </script>
         <title>RESTAURANTE </title>
   </head>
   
