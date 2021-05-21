ca563a322cb9676afa2215d034151ff590db7547
yairguevara12 <guevaradelgadoyair12@gmial.com>
2021-05-21 11:34:48 -0500

    yair

---
+++ controllers/confirmarcorreo.php
@@ -0,0 +1,73 @@
+<?php 
+require "PHPMailer/Exception.php";
+require "PHPMailer/PHPMailer.php";
+require "PHPMailer/SMTP.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+$oMail= new PHPMailer();
+
+/*$oMail->SMTPOptions = array(
+  'ssl' => array(
+      'verify_peer' => false,
+      'verify_peer_name' => false,
+      'allow_self_signed' => true
+  )
+);*/
+/*
+$oMail->SMTPDebug = 3 ; */
+/*$oMail->SMTPDebug = 2 ; en caso de errores*/
+$oMail->isSMTP();
+$oMail->SMTPAuth=true;
+/*$oMail->SMTPSecure="tls";*/
+$oMail->SMTPSecure="STARTTLS";
+/*$oMail->Host="plusvision.com.pe";*/
+/*$oMail->Host="mail.plusvision.com.pe";///esta mal*/
+/*$oMail->Host="superm107111.planetahosting.pe";*/
+
+//$oMail->Host="plusvisionperu.com";
+$oMail->Host="localhost";
+/*$oMail->Host="smtp.gmail.com";*/
+/* para ssl
+$oMail->Port=465 /587;*/
+/*$oMail->Port=26 ;*/
+$oMail->Port=25 ;
+$oMail->CharSet = "UTF-8" ; 
+/*
+$oMail->Username="ventas@plusvision.com.pe";
+$oMail->Password="@Vision2021@";*/
+$oMail->Username="informacion@plusvisionperu.com";
+$oMail->Password="infoplusvi2020";
+/*$oMail->Username="pepitosamoradelgadillo@gmail.com";
+$oMail->Password="pepitosamora1234";*/
+//destinatario
+
+//$oMail->setFrom("ventas@plusvision.com.pe","PLUS VISION PRUEBA");
+$oMail->setFrom("informacion@plusvisionperu.com","PLUS VISION PRUEBA");
+/*$oMail->addAddress("ventas@plusvision.com.pe" ,$nombre);*/
+//$oMail->addAddress("ventas@plusvision.com.pe" ,$nombre);/*esto esta mal*/
+$oMail->addAddress("informacion@plusvisionperu.com" ,$nombre);/*esto esta mal*/
+
+
+$oMail->SMTPKeepAlive = true; 
+$oMail->Mailer = "smtp"; 
+$oMail->Subject="<- IMPORTANTE -> SOLICITUD DE INFORMACION - PAGINA WEB";
+$oMail->isHTML(true);
+$oMail->msgHTML("NOMBRE : ".$nombre."<br>".
+                "TELEFONO:".$telefono."<br>".
+                "EMAIL : " . $email."<br>".
+                "RUC : " .$ruc."<br>".  
+                "SOLICITUD : ".$solicitud );
+ 
+if(!$oMail->send()){
+echo $oMail->ErrorInfo; 
+
+
+
+}
+
+
+
+?>
