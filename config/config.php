324502a1cf69474c91922aa07930a92e7b66aebe
yairguevara12 <guevaradelgadoyair12@gmial.com>
2021-06-04 09:37:04 -0500

    yair

--- config/config.php
+++ config/config.php
@@ -6,10 +6,6 @@
 //define("URL", "http://localhost/formacion/");
 define("URL", "http://localhost/formacion/formacionweb/");
 //DEFINIR LOS PARAMETROS DE LA BASE DE DATOS
-define("SERVIDOR","127.0.0.1");
-define("BASEDATOS","bd303");
-define("USUARIO","root");
-define("CLAVE","");
-define("CHARSET","UTF8");
+
 
 ?>
--- views/platos/index.php
+++ views/platos/index.php
@@ -6,6 +6,16 @@
 
 
 
+
+
+
+	<form action="<?php  echo constant('URL') ."platos/subido" ?>" method="POST" enctype="multipart/form-data">
+         <input type="file" name="image" />
+         <input type="submit"/>
+      </form>
+
+
+
 <?php
 	require_once("views/footer.php");
 
