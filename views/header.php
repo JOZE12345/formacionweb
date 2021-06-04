5c3d32c03a859a1f8b9117a8a6bb2f334138f635
JOZE12345 <ja8228228@gmail.com>
2021-06-04 15:25:06 -0500

    antonio

--- views/header.php
+++ views/header.php
@@ -8,7 +8,7 @@
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="<?php echo constant('URL') ?>public/css/estilo.css">
     <link rel="stylesheet" href="<?php echo constant('URL') ?>public/css/login2.css">
     <link rel="stylesheet" href="<?php echo constant('URL') ?>public/css/footer.css">
     <link rel="stylesheet" href="<?php echo constant('URL') ?>public/css/hover.css">
