cb4364bb9057be169c8140aa48a4ef78d5476699
yairguevara12 <guevaradelgadoyair12@gmial.com>
2021-06-04 10:39:47 -0500

    yair

--- views/editarrestaurante/index.php
+++ views/editarrestaurante/index.php
@@ -132,13 +132,59 @@ if(trim($recuperarrestaurante["nombrePermisoSalubridad"])!="No Tiene ningun perm
 </table>
 
 
-<img id ="plato" src="100" alt="100" class="container img-thumbnail"> 
+
+
+
+
+
+
+
+
+
+<div class="modal fade" id="confirmar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Confirme identidad</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+          <img  id ="plato" src="100" alt="100" class="container img-thumbnail"> 
+          </div>
+          
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button"  onclick="confirmarcontraseña()"  class="btn btn-primary">Enviar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 <script>
 	
 function cambiarplato(src){
 
   $("#plato").attr("src",src);
+
+  $("#confirmar").modal("show");
  
 }
 
